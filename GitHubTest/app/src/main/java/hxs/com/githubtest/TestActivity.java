8857807059417d9bdffcea0bcbe61597fecef233
enjoy <237309724@qq.com>
2016-06-18 14:18:44 +0800

    测试git代码提交

--- GitHubTest/app/src/main/java/hxs/com/githubtest/TestActivity.java
+++ GitHubTest/app/src/main/java/hxs/com/githubtest/TestActivity.java
@@ -1,7 +1,12 @@
 package hxs.com.githubtest;
 
+import android.app.Activity;
+
 /**
  * Created by huangxiangshou on 16/6/18.
  */
-public class TestActivity {
+public class TestActivity extends Activity{
+
+    public TestActivity() {
+    }
 }
