1e726de3900310433029fb98c92d2c4e9350388e
enjoy <237309724@qq.com>
2016-06-18 14:50:09 +0800

    添加SplashAtivity

--- GitHubTest/app/src/main/java/hxs/com/githubtest/SplashActivity.java
+++ GitHubTest/app/src/main/java/hxs/com/githubtest/SplashActivity.java
@@ -1,7 +1,15 @@
 package hxs.com.githubtest;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by huangxiangshou on 16/6/18.
  */
-public class SplashActivity {
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
